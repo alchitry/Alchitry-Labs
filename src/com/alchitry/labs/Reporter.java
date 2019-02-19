ec3bade4b0c15044ad408cba3b19e73e17c126a3
Justin Rajewski <justin@alchitry.com>
2019-02-19 10:24:12 -0700

    Fixed reported type for public source

--- src/com/alchitry/labs/Reporter.java
+++ src/com/alchitry/labs/Reporter.java
@@ -1,18 +1,5 @@
 package com.alchitry.labs;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
-import org.eclipse.swt.widgets.Shell;
-
 import com.alchitry.labs.gui.EmailMessage;
 
 public class Reporter {
@@ -20,8 +7,7 @@ public class Reporter {
 		// dummy call for public source
 	}
 
-	public static void reportException(Exception e, EmailMessage message) {
-		message.body += "\r\n\r\n" + Util.exceptionToString(e);
-		sendFeedback(message);
+	public static void reportException(Exception e) {
+		// dummy call for public source
 	}
 }
